b52fb5a5e2b572ab3f1d8088d4638e1a75edcb61
Tushar Samant <live@now-is.org>
2024-03-06 00:31:11 -0600

    Introduce drag functions

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -7,7 +7,7 @@ enum Mode {
 }
 
 type State = {
-	string: string;
+	string: string[];
 	height: number;
 	width: number;
 	cursor: number;
@@ -15,11 +15,35 @@ type State = {
 	moves: number;
 }
 
-export const state: State = {
-	string: 'the quick brown fox jumped over the lazy dog',
+export const initState: State = {
+	string: 'the quick brown fox jumped over the lazy dog'.split(''),
 	height: 4,
 	width: 8,
 	cursor: 0,
 	mode: Mode.Pan,
 	moves: 0,
 };
+
+export function moveRight(st: State) : State {
+	if (st.cursor >= st.string.length - 1) {
+		return st;
+	}
+	const letter = st.string[st.cursor];
+	st.string[st.cursor] = st.string[st.cursor+1];
+	st.string[st.cursor+1] = letter;
+	st.cursor += 1;
+	st.moves += 1;
+	return st;
+}
+
+export function moveLeft(st: State) : State {
+	if (st.cursor <= 0) {
+		return st;
+	}
+	const letter = st.string[st.cursor];
+	st.string[st.cursor] = st.string[st.cursor-1];
+	st.string[st.cursor-1] = letter;
+	st.cursor -= 1;
+	st.moves += 1;
+	return st;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,35 @@
 <script>
-	import { state } from '$lib/state.ts';
+	import { initState, moveRight, moveLeft } from '$lib/state.ts';
+
+	let state = initState;
 </script>
 
-<h1>home</h1>
-<p>{state.string}</p>
+<div class="app">
+<p>
+	{#each state.string as letter, i}
+		<span class="letter {i == state.cursor ? 'cursor' : ''}">{letter}</span>
+	{/each}
+</p>
+<p>MOVES: {state.moves}</p>
+
+<button on:click={() => state = moveLeft(state)}>move left</button>
+<button on:click={() => state = moveRight(state)}>move right</button>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+	.letter {
+		display: inline-block;
+		height: 1.5em;
+		width: 1.5em;
+		border-width: 1px;
+		border-style: solid;
+		border-color: #aaa;
+		margin-inline: 4px;
+	}
+	.letter.cursor {
+		font-weight: bold;
+		border-color: black;
+	}
+</style>
